d1d816cd294e2d2984d0dd0eb4d4b89b4b641010
20880115 <you@example.com>
2021-07-16 21:15:49 +0700

    1

---
+++ ds&algo/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Stack<String> stack = new Stack<String>();
+		
+		stack.push("1");
+		stack.push("2");
+		stack.push("3");
+		stack.push("4");
+		stack.push("5");
+
+		/*
+		 stack = LIFO data structure. Last-In First-Out
+        stores objects into a sort of "vertical tower"
+		 
+		  uses of stacks:
+   1. undo/redo features in text editors
+   2. moving back/forward through browser history
+   3. backtracking algorithms (maze, file directories)
+   4. calling functions (call stack)
+		 
+		 
+		 
+		 
+		stack.pop(); //remove last input
+			
+		System.out.println(stack.empty()); //check stack empty or not
+		
+		String myfavnumber = stack.pop(); // print the remove item
+		
+		System.out.println(stack.peek()); //print the last item not remove it
+		
+		
+		System.out.println(stack);
+		
+		System.out.println(stack.search("132465")); //return -1
+		*/
+		
+		
+
+	}
+
+}
